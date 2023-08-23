d77e714cb0f01c8f9996e4bbf4b3bda09ae77a44
Parul Pandey <parul.pandey85@gmail.com>
2023-08-23 11:37:30 -0700

    folder re-org

--- utils.py
+++ utils.py
@@ -3,16 +3,17 @@ from keras.utils import to_categorical
 
 ### Multi-class classification problem
 
-
 def create_dataset(n_samples, centers, n_features, cluster_std, random_state):
 
-	# generate 2d classification dataset
+	# generate n-d classification dataset
 	X, y = make_blobs(n_samples=n_samples, centers=centers, n_features=n_features, cluster_std=cluster_std, 
 		   random_state=random_state)
+	
 	# one hot encode output variable
 	y = to_categorical(y)
+	
 	# split into train and test
-	n_train = 500
+	n_train = n_samples*0.8
 	trainX, testX = X[:n_train, :], X[n_train:, :]
 	trainy, testy = y[:n_train], y[n_train:]
 	return trainX, trainy, testX, testy
