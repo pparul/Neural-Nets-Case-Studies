477631037ce58738227dc43034c58e93ebb2d1b8
pparul <parul.pandey85@gmail.com>
2023-10-12 10:52:30 -0700

    check pre-commits

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Neural-Nets-Case-Studies
 
-This repository focusses on understanding impact of different hyper-parameters of neural networks on accuracy and training speed. 
+This repository focusses on understanding impact of different hyper-parameters of neural networks on accuracy and training speed.
 1. Vanishing gradients: Impact of activation functions
-2. Step-length 
-
+2. Step-length
--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 from sklearn.datasets import make_blobs
 from keras.utils import to_categorical
 import numpy as np
-from autograd import grad 
+from autograd import grad
 from autograd import numpy as np
-from autograd import value_and_grad 
+from autograd import value_and_grad
 import matplotlib.pyplot as plt
 
 ### Multi-class classification problem
@@ -11,12 +11,12 @@ import matplotlib.pyplot as plt
 def create_dataset(n_samples, centers, n_features, cluster_std, random_state):
 
 	# generate n-d classification dataset
-	X, y = make_blobs(n_samples=n_samples, centers=centers, n_features=n_features, cluster_std=cluster_std, 
+	X, y = make_blobs(n_samples=n_samples, centers=centers, n_features=n_features, cluster_std=cluster_std,
 		   random_state=random_state)
-	
+
 	# one hot encode output variable
 	y = to_categorical(y)
-	
+
 	# split into train and test
 	n_train = n_samples*0.8
 	trainX, testX = X[:n_train, :], X[n_train:, :]
@@ -38,30 +38,30 @@ def gen_plot_varaiables(weight_1,weight_2, g):
     return w1_vals, w2_vals, func_vals
 
 
-def plot_contour_plots(w1_vals, w2_vals, weight_history, func_vals, alpha_choice):    
+def plot_contour_plots(w1_vals, w2_vals, weight_history, func_vals, alpha_choice):
     plt.figure(figsize = (8,3))
     plt.contour(w1_vals, w2_vals, func_vals)
     plt.title(f'learning rate: {alpha_choice}')
-    
+
     for j in range(len(weight_history)):
-        w_val = weight_history[j] # the results from Gradient descent and starting from an initial value 
+        w_val = weight_history[j] # the results from Gradient descent and starting from an initial value
         plt.scatter(w_val[0], w_val[1], linestyle='--', marker='o', s=50, c='r')
-    
+
         if j > 0:
                     pt1 = weight_history[j-1]
                     pt2 = weight_history[j]
-                    
+
                     # produce scalar for arrow head length
                     pt_length = np.linalg.norm(pt1 - pt2)
                     head_length = 0.1
                     alpha = (head_length - 0.35)/pt_length + 1
-                    
+
                     # if points are different draw error
                     if np.linalg.norm(pt1 - pt2) > head_length: #and arrows == True:
                         if np.ndim(pt1) > 1:
                             pt1 = pt1.flatten()
                             pt2 = pt2.flatten()
-                            
+
                         # draw color connectors for visualization
                         w_old = pt1
                         w_new = pt2
@@ -82,7 +82,7 @@ def gradient_descent(g,alpha_choice,max_its,w):
             alpha = 1/float(k)
         else:
             alpha = alpha_choice
-        
+
         # evaluate the gradient, store current weights and cost function value
         cost_eval,grad_eval = gradient(w)
         weight_history.append(w)
@@ -90,12 +90,11 @@ def gradient_descent(g,alpha_choice,max_its,w):
 
         # take gradient descent step
         w = w - alpha*grad_eval
-            
+
     # collect final weights
     weight_history.append(w)
-    # compute final cost function value via g itself (since we aren't computing 
-    # the gradient at the final step we don't get the final cost function value 
-    # via the Automatic Differentiatoor) 
-    cost_history.append(g(w))  
+    # compute final cost function value via g itself (since we aren't computing
+    # the gradient at the final step we don't get the final cost function value
+    # via the Automatic Differentiatoor)
+    cost_history.append(g(w))
     return weight_history,cost_history
-
